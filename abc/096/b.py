fbcb9102a6671375178923eaef9ed2751d5bf066
takey <taketakeyyy@gmail.com>
2018-06-20 12:42:15 +0900

    solved: a, b

--- abc/096/b.py
+++ abc/096/b.py
@@ -3,15 +3,15 @@ import math
 
 def solve(X):
 	ans = X
-	# b^p = ansã¨ãªã‚‹ansã‚’æ¢ã™
+	# b^p = ans‚Æ‚È‚éans‚ğ’T‚·
 	for b in range(2, X):
-		p = math.log(ans, b) # ç²¾åº¦ã§æ­»ã¬(log(100, 10)ãªã©)
+		p = math.log(ans, b) # ¸“x‚Å€‚Ê(log(100, 10)‚È‚Ç)
 		if float.is_integer(p):
 			return ans
 	return 1
 
 def solve2(X):
-	# å…¨æ¢ç´¢
+	# ‘S’Tõ
 	for x in range(X, 1, -1):
 		for p in range(2, x):
 			for b in range(2, x):
