8e9112290296c6709fa9faf7961a2b956c5c79a2
taketakeyyy <taketakeyyy@gmail.com>
2018-10-12 20:32:52 +0900

    solved: a, b

---
+++ abc/112/a.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+def main():
+    N = input()
+
+    if N == "1":
+        print("Hello World")
+        exit()
+    else:
+        print(int(input()) + int(input()))
+
+if __name__ == '__main__':
+    main()
