539960d36358b73b9c22cf2066bf5460ad857f80
taketakeyyy <taketakeyyy@gmail.com>
2019-03-01 23:42:40 +0900

    solved

--- abc/116/c.py
+++ abc/116/c.py
@@ -1,57 +1,21 @@
-# Timeout
-
-def update_stops(flowers, h, stops):
-    N = len(flowers)
-    for i in range(N):
-        if flowers[i] == h[i]:
-            stops[i] = True    
-    
-
-def do_water(ans, flowers, start, stop):
-    for i in range(start, stop):
-        flowers[i] += 1
-    ans += 1
-    return ans, flowers
-
-def is_end(stops):
-    for i in range(len(stops)):
-        if stops[i] is False:
-            return False
-    return True
 
 
 def solve():
+    # 解説放送を実装
     N = int(input())
     flowers = [0]*N
     h = [int(x) for x in input().split(" ")]
+    ans = h[0]
+    stack = h[0]
 
-    stops = [False]*N
-
-    ans = 0
-    while True:
-        update_stops(flowers, h, stops)
-        if is_end(stops):
-            print(ans)
-            return
-
-        start = 0
-        stop = N
-        while True:
-            for i in range(start, N):
-                if stops[i] is False:
-                    start = i
-                    break
-            for j in range(i, N):
-                if stops[j] is True:
-                    stop = j
-                    break
-
-            ans, flowers = do_water(ans, flowers, start, stop)
-
-            if stop == N:
-                break
-            
-            start = stop + 1
+    for i in range(1, N):
+        if h[i] <= stack:
+            stack = h[i]
+        else:
+            ans += (h[i] - stack)
+            stack = h[i]
+    
+    print(ans)
 
 
 if __name__ == "__main__":
