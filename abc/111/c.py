d832c4da3ed2a7d54baa758dfdf5ba7c9e139037
taketakeyyy <taketakeyyy@gmail.com>
2019-04-13 10:10:39 +0900

    solved

--- abc/111/c.py
+++ abc/111/c.py
@@ -1,65 +1,57 @@
-# -*- coding: utf-8 -*-
+# -*- coding:utf-8 -*-
 
-def main():
-    import collections
-
+def solve():
     n = int(input())
-    vs = list(map(int, input().split()))
+    V = [None]
+    V.extend(list(map(int, input().split(" "))))
+    
+    # 1種類の数字で構成されているか？
+    tmp = V[1]
+    flag = True
+    for i in range(2, n+1):
+        if tmp == V[i]:
+            continue
+        flag = False
+        break
+    if flag:
+        print(int(n/2))
+        return
 
-    vs_even = []
-    vs_odd = []
-    while True:
-        if len(vs) == 0:
-            break
-        vs_odd.append(vs.pop(0))
-        vs_even.append(vs.pop(0))
+    # 奇数番目のもので、最も多い数字は？
+    odd_dict = {}
+    for i in range(1, n+1, 2):
+        # 奇数番目のもので、最も多い数字を調べる
+        if V[i] in odd_dict.keys():
+            odd_dict[V[i]] += 1
+            continue
+        odd_dict[V[i]] = 1
 
-    if vs_even == vs_odd:
-        # 1種類の数からなる数列のとき
-        print(len(vs_even))
-        exit()
+    # 偶数番目のもので、最も多い数字は？
+    even_dict = {}
+    for i in range(2, n+1, 2):
+        # 偶数番目のもので、最も多い数字を調べる
+        if V[i] in even_dict.keys():
+            even_dict[V[i]] += 1
+            continue
+        even_dict[V[i]] = 1
 
-    most_evens = collections.Counter(vs_even).most_common(2)
-    most_odds = collections.Counter(vs_odd).most_common(2)
+    sorted_odds = sorted(odd_dict.items(), key=lambda x: x[1], reverse=True)
+    sorted_evens = sorted(even_dict.items(), key=lambda x: x[1], reverse=True)
 
-    len_vs_even = len(vs_even)
-    if most_evens[0][0] == most_odds[0][0]:
-        # 変換したら、1種類の数になってしまう
-        # ex.) vs = [1 2 1 2 1 2 2 2 2 2 2 2 2 2]
-        # ex.) vs = [1 1 1 1 1 1 3 2 3 2]
-        # ので、2番目に大きい数字で変換する
-        if most_evens[0][1] > most_odds[0][1]:
-            ans = 0
-            ans += len_vs_even - most_odds[1][1]
-            ans += len_vs_even - most_evens[0][1]
-            print(ans)
-            exit()
-        elif most_evens[0][1] == most_odds[0][1]:
-            if most_evens[1][1] >= most_odds[1][1]:
-                ans = 0
-                ans += len_vs_even - most_evens[1][1]
-                ans += len_vs_even - most_odds[0][1]
-                print(ans)
-                exit()
-            else:
-                ans = 0
-                ans += len_vs_even - most_odds[1][1]
-                ans += len_vs_even - most_evens[0][1]
-                print(ans)
-                exit()
+    # 変換後、1種類の数字にならないように気を付ける
+    if sorted_evens[0][0] == sorted_odds[0][0]:
+        if len(sorted_odds) == 1:
+            ans = n - sorted_odds[0][1] - sorted_evens[1][1]
+        elif len(sorted_evens) == 1:
+            ans = n - sorted_odds[1][1] - sorted_evens[0][1]
         else:
-            ans = 0
-            ans += len_vs_even - most_evens[1][1]
-            ans += len_vs_even - most_odds[0][1]
-            print(ans)
-            exit()
-    # 変換しても、2種類の数になるならば
-    ans = 0
-    ans += len_vs_even - most_evens[0][1]
-    ans += len_vs_even - most_odds[0][1]
+            ans1 = n - sorted_odds[0][1] - sorted_evens[1][1]
+            ans2 = n - sorted_odds[1][1] - sorted_evens[0][1]
+            ans = min(ans1, ans2)
+    else:
+        ans = n - sorted_evens[0][1] - sorted_odds[0][1]
+
     print(ans)
 
-
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    solve()
