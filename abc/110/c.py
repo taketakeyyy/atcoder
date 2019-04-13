f01c9e17ff84929e0a1285003a058c34df9ca742
taketakeyyy <taketakeyyy@gmail.com>
2019-04-13 10:35:01 +0900

    solved

--- abc/110/c.py
+++ abc/110/c.py
@@ -1,100 +1,57 @@
-# -*- coding: utf-8 -*-
+# -*- coding:utf-8 -*-
 
-def main():
-    """ TLE """
-    S = input()
+def calc(S, T):
+    # 差を計算する
+    ans = 0
+    for i in range(len(S)):
+        if S[i] != T[i]:
+            ans += 1
+    return ans
+
+
+def solve():
+    s = input()
+    S = [moji for moji in s]
     T = input()
 
-    if S == T:
+    # 文字の置き換えをして、差が減ってなかったら無理？
+    
+    diff = calc(S, T)
+    if diff == 0:
         print("Yes")
-        exit()
+        return
+    pre_diff = diff
 
-    # ある文字cがsとtで場所が半端に一致していたらNO
-    len_s = len(S)
-    for i in range(len_s):
-        flag1 = False
-        flag2 = False
-        fuitti = []
-        finds = []
-        start_i = i
-        while True:
-            find_i = T.find(S[i], start_i)
-            if find_i == -1:
+    while True:
+        # c1, c2を決める
+        for i in range(len(S)):
+            if S[i] != T[i]:
+                c1 = S[i]
+                c2 = T[i]
                 break
-            finds.append(find_i)
-            start_i = find_i
-        # findsを走査
-        for find in finds:
-            if S[find] == T[find]:
-                flag1 = True
-            else:
-                flag2 = True
-                fuitti.append(find)
-        if flag1 and flag2:
-            for f in fuitti:
-                find_f = T.find(S[f])
-                if find_f != -1:
-                    print("No")
-                    exit()
-    print("Yes")
-
-
-
-def main3():
-    """ TLE """
-    S = input()
-    T = input()
-
-    if S == T:
-        print("Yes")
-        exit()
-
-    # ある文字cがsとtで場所が半端に一致していたらNO
-    ans = S
-    len_s = len(S)
-    for i in range(len_s):
-        if ans[i] != T[i]:
-            c1 = ans[i]
-            c2 = T[i]
-            ans = ans.replace(c1, "?")
-            ans = ans.replace(c2, c1)
-            ans = ans.replace("?", c2)
-
-    if ans == T:
-        print("Yes")
-    else:
-        print("No")
-
-
-def main2():
-    """ TLE WA """
-    S = input()
-    T = input()
-
-    if S == T:
-        print("Yes")
-        exit()
-
-    # ある文字cがsとtで場所が半端に一致していたらNO
-    len_s = len(S)
-    for i in range(len_s):
-        if not S[i] in T:
+        
+        # 文字を置き換える
+        for i in range(len(S)):
+            if S[i] == c1:
+                S[i] = c2
+            elif S[i] == c2:
+                S[i] = c1
+        
+        # 差を計算する
+        diff = calc(S, T)
+        if diff == 0:
+            break
+        elif diff < pre_diff:
+            pre_diff = diff
             continue
-        flag1 = False
-        flag2 = False
-        for j in range(len_s):
-            if flag1 and flag2:
-                print("No")
-                exit()
-            if S[i] == T[j]:
-                if i == j:
-                    flag1 = True
-                    continue
-                flag2 = True
-                continue
+        else:
+            print("No")
+            return
     print("Yes")
 
 
 
-if __name__ == '__main__':
-    main()
+
+
+if __name__ == "__main__":
+    solve()
