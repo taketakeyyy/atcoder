bfcdde0efc2fa370533624e94ba538ab0c726499
taketakeyyy <taketakeyyy@gmail.com>
2019-05-05 22:39:31 +0900

    unsolve

--- abc/118/d.py
+++ abc/118/d.py
@@ -17,6 +17,8 @@ dp = max(dp[i-f(Aj)]+Aj | j=1~M)
 """ 解説2
 DP
 
+f(x) := xをつくるために使うマッチの本数
+
 dp[i] := i本使うときの桁数のmax
 
 dp[i] := max(dp[i-f(Aj)]+1 | j=1~M)
@@ -24,57 +26,56 @@ dp[i] := max(dp[i-f(Aj)]+1 | j=1~M)
 """
 from operator import attrgetter
 
-class Number():
-    def __init__(self, v, m):
-        self.value = v
-        self.match = m
+def num2match(x):
+    # 1,2,3,4,5,6,7,8,9を 1つ作るには、それぞれちょうど 2,5,5,4,5,6,3,7,6本のマッチを使う
+    if x == 1:
+        return 2
+    elif x == 2:
+        return 5
+    elif x == 3:
+        return 5
+    elif x == 4:
+        return 4
+    elif x == 5:
+        return 5
+    elif x == 6:
+        return 6
+    elif x == 7:
+        return 3
+    elif x == 8:
+        return 7
+    elif x == 9:
+        return 6
+
 
 def solve():
     N, M = list(map(int, input().split()))
     A = list(map(int, input().split()))
+    A.sort()
 
-    num_dict = {}
-    for a in A:
-        if a == 1:
-            num_dict[1] = 2
-        elif a == 2:
-            num_dict[2] = 5
-        elif a == 3:
-            num_dict[3] = 5
-        elif a == 4:
-            num_dict[4] = 4
-        elif a == 5:
-            num_dict[5] = 5
-        elif a == 6:
-            num_dict[6] = 6
-        elif a == 7:
-            num_dict[7] = 3
-        elif a == 8:
-            num_dict[8] = 7
-        elif a == 9:
-            num_dict[9] = 6
+    # dp[i] := マッチi本使うときの最大桁数
+    dp = [0] * (N+1)
+    dp[1] = 0
 
-    # 消費マッチ棒が少ない順かつ、数字が大きい順
-    num_matches = sorted(num_dict.items(), key=lambda x: x[0], reverse=True)
-    num_matches = sorted(num_matches, key=lambda x: x[1])
+    for i in range(2, N+1):
+        tmp = 0
+        for a in A:
+            if i-num2match(a) >= 0:
+                tmp = max(tmp, dp[i-num2match(a)] + 1)
+        dp[i] = tmp
+    
+    ans = ""
+    remain = dp[N] # 残りの桁数
+    match = N # 残りのマッチ本数
+    while(match > 0):
+        for i in range(M-1, -1, -1):
+            if dp[match-num2match(A[i])] == remain-1:
+                ans += str(A[i])
+                match -= num2match(A[i])
+                remain -= 1
+    print(ans)
 
-    import copy
-    if len(num_matches) >= 2:
-        copied_num_matches = copy.deepcopy(num_matches)
-        for i in range(len(copied_num_matches)-1):
-            if copied_num_matches[i][1] == copied_num_matches[i+1][1]:
-                # 消費マッチ数が同じなら、値が小さいやつは不要
-                num_matches.remove(copied_num_matches[i+1])    
-
-    # ・できるだけ桁数が大きいほうが良い
-    # ・マッチ棒はちょうど使い切る必要がある
-    while True:
-        for num_i in range(len(num_matches)):
-            now_num = num_matches[num_i][0]
-            loop = N%now_num
-            for loop_i in range(loop, 0, -1):
-                nuw_num*loop_i
-
+    
 
 if __name__ == "__main__":
     solve()
