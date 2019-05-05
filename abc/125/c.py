ae9b94f776fbe42b9759c4f1f13a141a633dba1c
taketakeyyy <taketakeyyy@gmail.com>
2019-05-05 20:56:53 +0900

    solved

--- abc/125/c.py
+++ abc/125/c.py
@@ -1,62 +1,89 @@
 # -*- coding:utf-8 -*-
+"""解説
+GCD, DP, 累積和
+
+「一つを選んで好きな数字に変えられる」とあるが、今回のケースでは「その数字を消す」と同じ。
+
+すべてのGCDを試す
+
+4 6 7 8 10 12 14
+
+という数列が与えられたとき、
+
+・4を消して、ほかの数字のGCDを計算
+
+6 7 8 10 12 14
+
+・6を消して、ほかの数字のGCDを計算
+
+4 7 8 10 12 14
+
+・7を消して、……
+
+というのを全列挙するが、N=10**5なので、O(N**2)では計算が間に合わない。
+
+なのでこれをもう少し高速でやりたい。
+
+GCDは計算順序を問わない性質を利用する。
+
+たとえば、8を消した場合
+
+4 6 7 8 10 12 14
+| 左 |× |   右  |
+
+・左のGCD
+・右のGCD
+を計算し、GCD(左のGCD, 右のGCD)をやってもよい。
+
+・左のGCDを考える
+GCD(4) = L[1]
+GCD(4 6) = L[2] = GCD(L[1], 6)
+GCD(4 6 7) = L[3] = GCD(L[2], 7)
+GCD(4 6 7 8) = L[4] = GCD(L[3], 8)
+GCD(4 6 7 8 10) = L[5] = GCD(L[4], 10)
+
+みたいな感じで計算できる。
+
+・右のGCDについても同様に計算してやる。
+
+この前計算ができたら、あとは累積和のように、どの数字を削除したときがGCDが最大になるのかを計算する。
+
+"""
+import copy
 
 def solve():
     N = int(input())
     A = list(map(int, input().split()))
 
-    # 他のAiにはあって、あるAiに唯一ない約数が答え
-    
-    candidates = []
+    def gcd(x, y):
+        """ 最大公約数を求める """
+        # x > y にしたい
+        if y > x:
+            x, y = y, x
+        if y == 0:
+            return x
+        return gcd(x%y, y)
 
-    if N == 2:
-        print(max(A[0], A[1]))
-        return
-
-    candidates = {}
-    # まずは0番目の奴の候補を考える
-    j = A[0]
-    for i in range(1, A[0]+1):
-        if i >= j:
-            break
-        if A[0]%i == 0:
-            candidates[i] = 1
-            j = A[0]//i
-            candidates[j] = 1
-
-    for key in candidates.keys():
-        if A[1]%key != 0:
-            candidates[key] -= 1
-
-    j = A[1]
-    for i in range(1, A[1]+1):
-        if i >= j:
-            break
-        if A[1]%i == 0:
-            if not i in candidates:
-                candidates[i] = 0
-            j = A[1]//i
-            if not j in candidates:
-                candidates[j] = 0
-
-    import copy
+    # L[i] := A[i]が消されたときの左側のGCD
+    L = [0]*(N+1)
+    L[1] = A[0]
     for i in range(2, N):
-        copied_candidates = copy.deepcopy(candidates)
-        for key in copied_candidates.keys():
-            if A[i]%key == 0:
-                pass
-            else:
-                candidates[key] -= 1
-                if candidates[key] < 0:
-                    candidates.pop(key)
+        L[i] = gcd(L[i-1], A[i-1])
+
+    # R[i] := A[i]が消されたときの右側のGCD
+    R = [0]*(N+1)
+    R[N-1] = 0
+    R[N-2] = A[N-1]
+    for i in range(N-3, -1, -1):
+        R[i] = gcd(R[i+1], A[i+1])
     
+    # 任意の数字を消したときの最大のGCDを求める
     ans = 0
-    for key in candidates.keys():
-        ans = max(ans, key)
-    
+    for i in range(N):
+        tmp = gcd(L[i], R[i])
+        ans = max(ans, tmp)
+
     print(ans)
-
-
-    
 
 if __name__ == "__main__":
     solve()
