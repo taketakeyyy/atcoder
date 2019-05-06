f1831e9ea2b5780d974e37619c858e833462ddf1
taketakeyyy <taketakeyyy@gmail.com>
2019-05-06 13:19:50 +0900

    solved

--- abc/107/c.py
+++ abc/107/c.py
@@ -1,50 +1,49 @@
 # -*- coding:utf-8 -*-
-import copy
 
-INF = 2*(10**8)+1
-memo = INF  # メモ化用変数
+"""解説
+https://www.youtube.com/watch?v=5O4k1n5Ejzw
 
+区間を考える問題
+
+-30 -10 0 10 20 50
+
+・K = 3のとき、
+
+0座標を必ず含める、ある連続するK個のロウソクについて考える
+
+・-30 -10 0 10
+・-10 0 10 20
+・0 10 20 50
+
+左端と右端に到達する最小値を求めていけばよくて、間のロウソクは無視してよい（必ず移動中に火をつけるので）
+
+すべての場合について求めて、その中の最小値が答え
+"""
 def solve():
-    N, K = list(map(int, input().split(" ")))
-    X = [0]
-    X.extend(list(map(int, input().split(" "))))
+    N, K = list(map(int, input().split()))
+    X = list(map(int, input().split()))
+    if 0 in X:
+        # 0座標にロウソクがあるなら、火をつけるロウソクをひとつ減らしてよい
+        K -= 1
+    else:
+        X.append(0)
     X.sort()
 
-    # 幅優先探索すればいいんじゃね？
+    origin_i = X.index(0)
+    left_i = max(origin_i-K, 0)
 
-    def dfs(depth, cost, i, Xs):
-        global memo
-        if depth == K:
-            memo = min(cost, memo)
-            return cost
-        
-        if cost >= memo:
-            # 探索を打ち切る
-            return INF
-        
-        ret1, ret2 = INF, INF
-        if i+1 < len(Xs):
-            # 右に移動
-            tmpCost = abs(Xs[i] - Xs[i+1])
-            tmpXs = copy.deepcopy(Xs)
-            del tmpXs[i]
-            ret1 = dfs(depth+1, cost+tmpCost, i, tmpXs)
-        if i-1 >= 0:
-            # 左に移動
-            tmpCost = abs(Xs[i] - Xs[i-1])
-            tmpXs = copy.deepcopy(Xs)
-            del tmpXs[i]
-            ret2 = dfs(depth+1, cost+tmpCost, i-1, tmpXs)
-        
-        return min(ret1, ret2)
-
-    # 0座標のインデックスは？
-    for i in range(len(X)):
-        if X[i] == 0:
+    INF = 10**12
+    ans = INF
+    while left_i <= origin_i:
+        if left_i+K < len(X):
+            tmp1 = abs(X[left_i]-0)*2 + abs(X[left_i+K]-0)
+            tmp2 = abs(X[left_i]-0) + abs(X[left_i+K]-0)*2
+        else:
             break
-    ans = dfs(0, 0, i, X)
+        ans = min(ans, tmp1, tmp2)
+        left_i += 1
     print(ans)
-
 
 if __name__ == "__main__":
     solve()
+
