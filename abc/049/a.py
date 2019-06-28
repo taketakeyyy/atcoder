138107d2c85cadcac54eb5cbd1f1e7f94fa9b69c
taketakeyyy <taketakeyyy@gmail.com>
2019-06-28 09:58:42 +0900

    solved

---
+++ abc/049/a.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+
+def solve():
+    c = input()
+    
+    if c in ("a", "i", "u", "e", "o"):
+        print("vowel")
+    else:
+        print("consonant")
+
+
+if __name__ == "__main__":
+    solve()
