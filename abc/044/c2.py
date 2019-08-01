30d11bd24ccfa7679d249d2cc10f9cada579afa9
taketakeyyy <taketakeyyy@gmail.com>
2019-08-01 11:52:39 +0900

    fix: comment

--- abc/044/c2.py
+++ abc/044/c2.py
@@ -1,31 +1,23 @@
 # -*- coding:utf-8 -*-
-"""解説
-dp[j][k][s] := x1,...,xjから k枚選んで xiの合計をsにするような選び方の総数
-
-[@lilium氏]
-https://atcoder.jp/contests/abc044/submissions/5887235
-"""
 
 def solve():
     N, A = list(map(int, input().split()))
     X = list(map(int, input().split()))
 
-    max_x = max(X)
     dp = [[[0 for _ in range(4000+1)] for __ in range(N+1)] for ___ in range(N+1)]
 
-    dp[0][0][0] = 1
-
-    # dp[i][j][k] := i枚のうちj枚を選んで、和がkになる組み合わせ
     """
     dp[i][j][k] := i枚のうち、k枚のカードを選んで、選んだカードの総和がkになる選び方の総数
     i: i枚目のカード
     j: j枚のカードを選んだ
     k: 選んだカードの総和
     """
+    dp[0][0][0] = 1
+
     for i in range(N):
         for j in range(N):
             for k in range(4000+1):
-                if dp[i][j][k] != 0:
+                if (k+X[i] < 4000+1) :  # TLEして草
                     dp[i+1][j][k] += dp[i][j][k]  # i枚目を選ばなかった
                     dp[i+1][j+1][k+X[i]] += dp[i][j][k]  # i枚目を選んだ
 
@@ -33,6 +25,7 @@ def solve():
     for j in range(1, N+1):
         ans += dp[N][j][j*A]
     print(ans)
+
 
 if __name__ == "__main__":
     solve()
