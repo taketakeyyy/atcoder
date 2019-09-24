220f6506d616ea19de782228fc6da8d1e05d408b
taketakeyyy <taketakeyyy@gmail.com>
2019-09-24 14:48:30 +0900

    solved

--- abc/137/d.py
+++ abc/137/d.py
@@ -1,29 +1,36 @@
 # -*- coding:utf-8 -*-
 import sys
+import heapq
+from collections import defaultdict
 
-class Job():
-    """アルバイト"""
-    def __init__(self, a, b):
-        self.after_days = a # a日後に報酬をもらえる
-        self.rewards = b    # 報酬
+"""解説PDF
+(1) 0日目から考えると、最初に選べるアルバイトが多すぎて、どれを適切に選んでいけばよいかわからない
 
+(2) M日後から考えると、M日後からi日前には、A_i<=iとなるアルバイトしか選べないので、これで考えると貪欲的に解ける
+
+"""
 def solve():
     N, M = list(map(int, sys.stdin.readline().split()))
+    jobs = defaultdict(lambda: [])
 
-    jobs = []
-    for _ in range(N):
-        a, b = list(map(int, sys.stdin.readline().split()))
-        jobs.append(Job(a,b))
-
-    # 優先度付きキューで解く。
-    # 解説放送とかPDFとかみるべし
-    dp = [[0]*(M+1) for _ in range(N)]
-    dp[0][0] = 0
+    # jobs[a] := 報酬がa日後に貰えるアルバイトのリスト
     for i in range(N):
-        for j in range(M+1):
-            dp[i][j] = dp[]
+        a, b = list(map(int, sys.stdin.readline().split()))
+        jobs[a].append(b)
 
+    ans = 0
+    heap = []  # アルバイト候補
+    for i in range(1, M+1):
+        # M-i日目に何のアルバイトをすればよいかを考える
+        for b in jobs[i]:
+            # 候補の追加
+            heapq.heappush(heap, -b)  # 最大値を根にするために、マイナスをかける
 
+        if len(heap) != 0:
+            # 候補の中で報酬が最大値を取り出す
+            ans += -heapq.heappop(heap)
+
+    print(ans)
 
 
 if __name__ == "__main__":
