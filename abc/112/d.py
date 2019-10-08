32e883eeb66a6e168e1df6dd495e72fb972d8ed5
taketakeyyy <taketakeyyy@gmail.com>
2019-10-08 15:13:14 +0900

    solved

--- abc/112/d.py
+++ abc/112/d.py
@@ -8,9 +8,8 @@ https://youtu.be/CURWJ1Dr44A?t=1708
 def solve():
     N, M = list(map(int, input().split()))
 
-    divs = set()  # Mの約数を格納する
-
     # Mの約数をすべて調べる
+    divs = set()  # Mの約数を格納する
     n = 1
     while n*n <= M:
         if M%n == 0:
@@ -19,10 +18,10 @@ def solve():
             divs.add(M//n)
         n += 1
 
-    # Mの約数dの中で、d*N <= Mを満たす最大のものが答え
+    # Mの約数dの中で、M >= d*Nを満たす最大のものが答え
     ans = 0
     for d in divs:
-        if d*N <= M:
+        if M >= d*N:
             ans = max(ans, d)
 
     print(ans)
@@ -31,15 +30,17 @@ def solve():
 def solve2():
     N, M = list(map(int, input().split()))
 
-    divs = set()
-
+    # Mの約数をすべて調べる
+    divs = set()  # Mの約数を格納する
     n = 1
     while n <= M**(1/2):
         if M%n == 0:
+            # nがMの約数ならば
             divs.add(n)
             divs.add(M//n)
         n += 1
 
+    # Mの約数dの中で、M >= d*Nを満たす最大のものが答え
     ans = 0
     for d in divs:
         if M >= d*N:
