226d72c2bbe50fa1bb45139070826708f8be226f
taketakeyyy <taketakeyyy@gmail.com>
2020-03-16 00:59:43 +0900

    solved

---
+++ abc/152/a.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+
+def solve():
+    N, M = list(map(int, input().split()))
+    if N == M:
+        print("Yes")
+    else:
+        print("No")
+
+if __name__ == "__main__":
+    solve()
