904e0f1f56a80d4373d33c0aa7f8c7c0d4fdacb7
taketakeyyy <taketakeyyy@gmail.com>
2020-05-24 00:48:58 +0900

    solved

--- abc/144/d.py
+++ abc/144/d.py
@@ -1,39 +1,24 @@
 # -*- coding:utf-8 -*-
-import math
-
 
 def solve():
-    a, b, x = map(int, input().split())
-    rest_v = (a**2)*b - x
+    import math
 
-    # 傾けたときの直角三角形の1辺はa, もう片方をcとすると、
-    # rest_v = (a*c/2)*a
-    # よって、
-    c = 2 * rest_v / (a**2)
+    a, b, x = list(map(int, input().split()))
 
-    # tanθ = a/c
-    tan_theta = a/c
-    ans = math.degrees(math.atan(tan_theta))
-    print(ans)
+    if x > a*a*b/2:
+        s = x/a
+        # a*b-s = a*h*(1/2)
+        h = (a*b-s)*2/a
+        theta = math.atan2(h, a)
+        ans = math.degrees(theta)
+        print(ans)
 
-    return
-
-    # 直角三角形の面積Sは、
-    S = a*c/2
-
-    # 直角三角形の3辺をそれぞれa, c, dとする
-    # dの長さは三平方の定理より、
-    d = (a**2 + c**2)**(1/2)
-
-    # 求めたい角度は、dとcがなす角度θ。
-    # 三角形の面積の、2辺とあいだの角度の公式より、
-    # S = d*c*sinθ / 2
-    # sinθのθを求めるには、逆正弦のarcsinを使って求める。
-    sin_theta = (S*2)/(d*c)
-    print(sin_theta)
-    ans = math.degrees(math.asin(sin_theta))
-
-    print(ans)
+    else:
+        s = x/a
+        h = (s*2)/b
+        theta = math.atan2(h, b)
+        ans = 180-90-math.degrees(theta)
+        print(ans)
 
 
 if __name__ == "__main__":
