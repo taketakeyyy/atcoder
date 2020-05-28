1dd494cfe199dd7e3e1446f5371b456b5c9f10c0
taketakeyyy <taketakeyyy@gmail.com>
2020-05-28 23:49:47 +0900

    solved

---
+++ abc/167/a.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+
+def solve():
+    S = input()
+    T = input()
+
+    if S == T[0:-1]:
+        print("Yes")
+    else:
+        print("No")
+
+if __name__ == "__main__":
+    solve()
