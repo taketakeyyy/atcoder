dcb13f261b22808d8b9a56d3ae3030f920d53552
taketakeyyy <taketakeyyy@gmail.com>
2020-09-21 19:08:15 +0900

    solved

--- abc/086/d.py
+++ abc/086/d.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 
 def solve():
+    # TLS
     import numpy as np
     N, K = list(map(int, input().split()))
     grid = [[0 for _ in range(2*K)] for __ in range(K)]
@@ -23,17 +24,19 @@ def solve():
             x, y = x-K, y+K
         grid[x][y] += 1  # 希望の位置を+1にする
 
+
     # 2次元累積和rを作る
     r = [[0 for _ in range(2*K+1)] for __ in range(K+1)]
     r[1][1] = grid[0][0]
     for x in range(1, K):
-        r[x+1][0] = r[x][0] + grid[x][0]
+        r[x+1][1] = r[x][1] + grid[x][0]
     for y in range(1, 2*K):
-        r[0][y+1] = r[0][y] + grid[0][y]
+        r[1][y+1] = r[1][y] + grid[0][y]
 
     for x in range(1, K):
         for y in range(1, 2*K):
             r[x+1][y+1] = r[x+1][y] + r[x][y+1] - r[x][y] + grid[x][y]
+
 
     def calc(x1, y1, x2, y2):
         """[(x1,y1), (x2,y2)]の範囲の累積和を返す"""
@@ -44,16 +47,55 @@ def solve():
     for x in range(K):
         for y in range(2*K):
             total = 0
-            if y+K >= 2*K and x+K >= 2*K:
+            if y+K-1 >= 2*K and x+K-1 >= K:
                 # xとyが境界をまたぐとき
                 x1, y1 = x, y
-                x2, y2 = 2*K-1, 2*K-1
+                x2, y2 = K-1, 2*K-1
                 total += calc(x1, y1, x2, y2)
 
-            elif y+K >= 2*K:
+                x3, y3 = x, 0
+                x4, y4 = K-1, (y+K-1)%(2*K)
+                total += calc(x3, y3, x4, y4)
+
+                x5, y5 = 0, y4+1
+                x6, y6 = x1-1, y1-1
+                total += calc(x5, y5, x6, y6)
+
+            elif y+K-1 >= 2*K:
                 # yのみが境界をまたぐとき
+                x1, y1 = 0, y
+                x2, y2 = K-1, 2*K-1
+                total += calc(x1, y1, x2, y2)
+
+                x3, y3 = 0, 0
+                x4, y4 = K-1, (y+K-1)%(2*K)
+                total += calc(x3, y3, x4, y4)
+
+            elif x+K-1 >= K:
+                # xのみが境界をまたぐとき
                 x1, y1 = x, y
-                x2, y2 = x+K
+                x2, y2 = K-1, y+K-1
+                total += calc(x1, y1, x2, y2)
+
+                if y2+1 < 2*K:
+                    x3, y3 = 0, y2+1
+                    x4, y4 = x-1, 2*K-1
+                    total += calc(x3, y3, x4, y4)
+
+                    x5, y5 = 0, 0
+                    x6, y6 = x-1, y-1
+                    total += calc(x5, y5, x6, y6)
+                else:
+                    x3, y3 = 0, y2+1
+                    x4, y4 = x-1, 2*K-1
+                    total += calc(x3, y3, x4, y4)
+            else:
+                # xもyもまたがないとき
+                x1, y1 = x, y
+                x2, y2 = x+K-1, y+K-1
+                total += calc(x1, y1, x2, y2)
+            ans = max(ans, total)
+    print(ans)
 
 
 if __name__ == "__main__":
