92ebab79a7e4556e47f42cb9fe0a9554ae3d8dc4
taketakeyyy <taketakeyyy@gmail.com>
2020-12-14 00:10:09 +0900

    solved

--- abc/179/d.cpp
+++ abc/179/d.cpp
@@ -41,33 +41,7 @@ istream& operator>>(istream& is, mint& a) { return is >> a.x;}
 ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
 
 
-void solve(){
-    int N, K;
-    cin >> N >> K;
-    vector<int> l(K), r(K);
-    rep(i, K) cin >> l[i] >> r[i];
-
-    // dp[i] := 左からiマス目にいるときの、行き方の数
-    vector<mint> dp(N+1, 0);     // dp
-    vector<mint> dpsum(N+1, 0);  // 累積和
-    dp[1] = 1;
-    dpsum[1] = 1;
-    for (int i=2; i<=N; i++) {
-        rep(j, K) {
-            // [l[j], r[j]]
-            int li = i-r[j];
-            int ri = i-l[j];
-            if (ri < 0) continue;
-            li = max(li, 1);  // 負になると困るので
-            dp[i] += dpsum[ri]-dpsum[li-1]; // dp[li]~dp[ri]
-        }
-        dpsum[i] = dpsum[i-1]+dp[i];
-    }
-    cout << dp[N] << endl;
-}
-
-
-void solve2() {
+void solve() {
   int N, K;
   cin >> N >> K;
   vector<int> l(K), r(K);
@@ -87,6 +61,41 @@ void solve2() {
     }
     dpsum[i] = dpsum[i-1] + dp[i];
   }
+  cout << dp[N] << endl;
+}
+
+
+void solve2 () {
+  int N, K;
+  cin >> N >> K;
+  vector<pair<int, int>> kukans(K);
+  for (int i=0; i<K; i++) {
+    int l, r;
+    cin >> l >> r;
+    kukans[i] = make_pair(l, r);
+  }
+
+  vector<int> dp(N+1, 0);  // dp[i] := iマスにいるときの場合の数
+  vector<int> sums(N+1, 0);  // sums[i] := iマス以下までの累積和（[l,r]は連続区間なので累積和で一気に足せる）
+  dp[0] = 0; dp[1] = 1;
+  sums[0] = 0; sums[1] = dp[1];
+  for (int i=2; i<N+1; i++) {
+    // もらうDP
+    for (auto p : kukans) {
+      int l, r;
+      l = p.first; r = p.second;
+      int ll, rr;
+      rr = max(i-l, 0);
+      ll = max(i-r-1, 0);
+      // dp[i] += (sums[rr]-sums[ll])%MOD;  // C++の引き算のMODは気をつける https://qiita.com/drken/items/3b4fdf0a78e7a138cd9a
+      int tmp = (sums[rr]-sums[ll])%MOD;
+      if (tmp < 0) tmp += MOD;
+      dp[i] += tmp;
+      dp[i] %= MOD;
+    }
+    sums[i] = (sums[i-1] + dp[i]) % MOD;
+  }
+
   cout << dp[N] << endl;
 }
 
