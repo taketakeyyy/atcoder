60c839587fdb3ab54d02db3548312e1ae8a2c808
taketakeyyy <taketakeyyy@gmail.com>
2021-01-23 19:50:30 +0900

    add comment

--- abc/160/e.py
+++ abc/160/e.py
@@ -6,9 +6,10 @@ def solve():
     Q = list(map(int, input().split()))
     R = list(map(int, input().split()))
     P.sort(), Q.sort(), R.sort()
-    ALL = P[len(P)-X:] + Q[len(Q)-Y:] + R
+    ALL = P[len(P)-X:] + Q[len(Q)-Y:] + R  # 赤いリンゴはたかだかX個しか食べたら駄目なので、大きいX個のみを抽出する。緑色も同様。
     ALL.sort(reverse=True)
     print(sum(ALL[0:X+Y]))
+
 
 if __name__ == "__main__":
     solve()
