cdc211d725b2b5158456e411bf6235e88ef108a0
taketakeyyy <taketakeyyy@gmail.com>
2021-01-26 00:53:23 +0900

    solved

--- abc/173/e.cpp
+++ abc/173/e.cpp
@@ -5,42 +5,107 @@
 #define rep(i,n) for (int i = 0; i < (n); ++i)
 using namespace std;
 using ll = long long;
-using P = pair<int,int>;
+using pii = pair<int,int>;
+const int INF = 1001001001;
+void chmax(int& x, int y) { x = max(x,y); }
+void chmin(int& x, int y) { x = min(x,y); }
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x=0):x((x%MOD+MOD)%MOD){}
+    mint operator-() const { return mint(-x);}
+    mint& operator+=(const mint a) {
+        if ((x += a.x) >= MOD) x -= MOD;
+        return *this;
+    }
+    mint& operator-=(const mint a) {
+        if ((x += MOD-a.x) >= MOD) x -= MOD;
+        return *this;
+    }
+    mint& operator*=(const mint a) { (x *= a.x) %= MOD; return *this;}
+    mint operator+(const mint a) const { return mint(*this) += a;}
+    mint operator-(const mint a) const { return mint(*this) -= a;}
+    mint operator*(const mint a) const { return mint(*this) *= a;}
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t>>1);
+        a *= a;
+        if (t&1) a *= *this;
+        return a;
+    }
+
+    // for prime MOD
+    mint inv() const { return pow(MOD-2);}
+    mint& operator/=(const mint a) { return *this *= a.inv();}
+    mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
 
 
 void solve(){
     int N, K; cin >> N >> K;
-    vector<int> plus;
-    vector<int> minus;
+    vector<ll> As(N);
+    vector<ll> plus;
+    vector<ll> minus;
+    int minus_num = 0, plus_num = 0;
     for (int i=0; i<N; i++) {
-        int a;
-        cin >> a;
-        if (a<0) {
-            minus.push_back(a);
+        cin >> As[i];
+        if (As[i]<0) {
+            minus_num++;
+            minus.push_back(As[i]);
         }
         else {
-            plus.push_back(a);
+            plus_num++;
+            plus.push_back(As[i]);
         }
     }
 
-    sort(plus.begin(), plus.end());
-    reverse(plus.begin(), plus.end());
-    sort(minus.begin(), minus.end());
-    reverse(minus.begin(), minus.end());
+    bool ok = false;  // 正にできるか？
+    if (plus_num > 0) {
+        if (N==K) ok = (minus_num%2==0);
+        else ok = true;
+    }
+    else {
+        ok = (K%2==0);
+    }
 
-    vector<int> one = plus;  // コスト1消費するもの
-    vector<int> two;  // コスト2消費するもの
-    for (int i=0; i<minus.size(); i++) {
-        if(i+1 < minus.size()) {
-            two.push_back((minus[i]*minus[i+1])%MOD);
+    mint ans = 1;
+    if (!ok) {
+        // 正にできない場合、絶対値が小さい順に取る
+        sort(As.begin(), As.end(), [](int x, int y) {return abs(x) < abs(y);} );
+        for (int i=0; i<K; i++) {
+            ans *= As[i];
         }
     }
+    else {
+        // 正にできる場合、2個ずつとっていく（ただしKが奇数のときは、正からまず1個とる）
+        sort(plus.rbegin(), plus.rend());
+        sort(minus.begin(), minus.end());
+        int pi = 0, mi = 0;
+        int cost = 0;
+        if (K%2==1 && plus.size()!=0) {
+            ans *= plus[pi];
+            pi++;
+            cost ++;
+        }
 
-    vector<int> dp(K,0);  // dp[i]:=コストiのときの作れる最大値
-    dp[1] = dp[0]+one[0];
-    for (int i=1; i<K; i++) {
+        vector<ll> dbl;  // 2個ずつの配列
+        for (; pi+1<plus.size(); pi+=2) {
+            dbl.push_back(plus[pi]*plus[pi+1]);
+        }
+        for (; mi+1<minus.size(); mi+=2) {
+            dbl.push_back(minus[mi]*minus[mi+1]);
+        }
+        sort(dbl.rbegin(), dbl.rend());
 
+        int i=0;
+        while(cost!=K) {
+            ans *= dbl[i];
+            i++;
+            cost += 2;
+        }
     }
+    cout << ans << endl;
 }
 
 
