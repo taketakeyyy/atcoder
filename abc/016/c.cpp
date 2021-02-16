cc98ca1b234def8ea0ad7543ec8fac89ab322261
taketakeyyy <taketakeyyy@gmail.com>
2021-02-17 06:18:07 +0900

    solved

---
+++ abc/016/c.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+#define _USE_MATH_DEFINES  // M_PI等のフラグ
+#define MOD 1000000007
+#define COUNTOF(array) (sizeof(array)/sizeof(array[0]))
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using pii = pair<int,int>;
+const int INF = 1001001001;
+void chmax(int& x, int y) { x = max(x,y); }
+void chmin(int& x, int y) { x = min(x,y); }
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x=0):x((x%MOD+MOD)%MOD){}
+    mint operator-() const { return mint(-x);}
+    mint& operator+=(const mint a) {
+        if ((x += a.x) >= MOD) x -= MOD;
+        return *this;
+    }
+    mint& operator-=(const mint a) {
+        if ((x += MOD-a.x) >= MOD) x -= MOD;
+        return *this;
+    }
+    mint& operator*=(const mint a) { (x *= a.x) %= MOD; return *this;}
+    mint operator+(const mint a) const { return mint(*this) += a;}
+    mint operator-(const mint a) const { return mint(*this) -= a;}
+    mint operator*(const mint a) const { return mint(*this) *= a;}
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t>>1);
+        a *= a;
+        if (t&1) a *= *this;
+        return a;
+    }
+
+    // for prime MOD
+    mint inv() const { return pow(MOD-2);}
+    mint& operator/=(const mint a) { return *this *= a.inv();}
+    mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+
+void solve(){
+    ll N, M; cin >> N >> M;
+    vector<set<ll>> friends(N);
+    rep(i, M) {
+        ll A, B;
+        cin >> A >> B;
+        A--; B--;
+        friends[A].insert(B);
+        friends[B].insert(A);
+    }
+
+    rep(i, N) {
+        set<ll> ans_set;
+        for (auto x: friends[i]) {
+            // 友達
+            if (x==i) continue;
+            for (auto y: friends[x]) {
+                // 友達の友達
+                if (friends[i].count(y) || y==i) continue;
+                ans_set.insert(y);
+            }
+        }
+        cout << ans_set.size() << endl;
+    }
+
+}
+
+
+int main(int argc, char const *argv[]){
+    solve();
+    return 0;
+}
