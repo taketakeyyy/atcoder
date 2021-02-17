ec0d1939483de92027813e2517dc225049b07b1a
taketakeyyy <taketakeyyy@gmail.com>
2021-02-18 02:24:14 +0900

    solved

---
+++ arc/033/b.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#define _USE_MATH_DEFINES  // M_PI等のフラグ
+#define MOD 1000000007
+#define COUNTOF(array) (sizeof(array)/sizeof(array[0]))
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using pii = pair<int,int>;
+const int INF = 1001001001;
+void chmax(int& x, int y) { x = max(x,y); }
+void chmin(int& x, int y) { x = min(x,y); }
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x=0):x((x%MOD+MOD)%MOD){}
+    mint operator-() const { return mint(-x);}
+    mint& operator+=(const mint a) {
+        if ((x += a.x) >= MOD) x -= MOD;
+        return *this;
+    }
+    mint& operator-=(const mint a) {
+        if ((x += MOD-a.x) >= MOD) x -= MOD;
+        return *this;
+    }
+    mint& operator*=(const mint a) { (x *= a.x) %= MOD; return *this;}
+    mint operator+(const mint a) const { return mint(*this) += a;}
+    mint operator-(const mint a) const { return mint(*this) -= a;}
+    mint operator*(const mint a) const { return mint(*this) *= a;}
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t>>1);
+        a *= a;
+        if (t&1) a *= *this;
+        return a;
+    }
+
+    // for prime MOD
+    mint inv() const { return pow(MOD-2);}
+    mint& operator/=(const mint a) { return *this *= a.inv();}
+    mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+
+void solve(){
+    ll Na, Nb; cin >> Na >> Nb;
+    set<ll> mp1, mp2;
+    for (int i=0; i<Na; i++) {
+        ll a; cin >> a;
+        mp1.insert(a);
+    }
+    ll bunbo=mp1.size();
+    ll bunshi=0;
+    for (int i=0; i<Nb; i++) {
+        ll a; cin >> a;
+        if (mp1.count(a)) {
+            bunshi++;
+        }
+        else {
+            bunbo++;
+        }
+    }
+
+    double ans = (double)bunshi/bunbo;
+    printf("%.8lf\n", ans);
+}
+
+
+int main(int argc, char const *argv[]){
+    solve();
+    return 0;
+}
