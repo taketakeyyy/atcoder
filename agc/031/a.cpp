b546d0cdf9ea2b168a4965b94f1c042066895fd4
taketakeyyy <taketakeyyy@gmail.com>
2021-02-24 21:58:57 +0900

    solved

---
+++ agc/031/a.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+#define _USE_MATH_DEFINES  // M_PI等のフラグ
+#define MOD 1000000007
+#define COUNTOF(array) (sizeof(array)/sizeof(array[0]))
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define intceil(a,b) (a+(b-1))/b
+using namespace std;
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<long,long>;
+const long long INF = LONG_LONG_MAX - 1001001001;
+void chmax(int& x, int y) { x = max(x,y); }
+void chmin(int& x, int y) { x = min(x,y); }
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x=0):x((x%MOD+MOD)%MOD){}
+    mint operator-() const { return mint(-x);}
+    mint& operator+=(const mint a) {
+        if ((x += a.x) >= MOD) x -= MOD;
+        return *this;
+    }
+    mint& operator-=(const mint a) {
+        if ((x += MOD-a.x) >= MOD) x -= MOD;
+        return *this;
+    }
+    mint& operator*=(const mint a) { (x *= a.x) %= MOD; return *this;}
+    mint operator+(const mint a) const { return mint(*this) += a;}
+    mint operator-(const mint a) const { return mint(*this) -= a;}
+    mint operator*(const mint a) const { return mint(*this) *= a;}
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t>>1);
+        a *= a;
+        if (t&1) a *= *this;
+        return a;
+    }
+
+    // for prime MOD
+    mint inv() const { return pow(MOD-2);}
+    mint& operator/=(const mint a) { return *this *= a.inv();}
+    mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+template<typename T>
+T mod_pow(T a, T n, T m) {
+    /** a^{n} mod m の計算を、繰り返し二乗法で求める
+     *  計算量：O(log n)
+     **/
+    if (n==0) return 1;
+    T res = mod_pow(a*a%m, n/2, m);
+    if (n&1) res = res * a % m;
+    return res;
+}
+
+template <typename T>
+T combination_mod(T n, T r, T mod) {
+    /*** nCrを O(min(n-r, r)) で高速に計算する ***/
+    auto inv = [mod](T x) { return mod_pow<T>(x, mod-2, mod); };
+
+    r = min(n-r, r);
+    T bunsi=1, bunbo=1;
+    for (T i=0; i<r; i++) {
+        bunsi = bunsi*(n-i)%mod;
+        bunbo = bunbo*(i+1)%mod;
+    }
+    return (bunsi*inv(bunbo))%mod;
+}
+
+
+void solve(){
+    ll N; cin >> N;
+    string S; cin >> S;
+    map<char,ll> mp;  // 各文字が何文字出現したか
+    for (char c: S) {
+        mp[c]++;
+    }
+
+    mint ans = mint(1);
+    for(auto [key, value]: mp) {
+        ans *= (value+1);
+    }
+    ans -= 1;
+    cout << ans << endl;
+}
+
+
+int main(int argc, char const *argv[]){
+    solve();
+    return 0;
+}
