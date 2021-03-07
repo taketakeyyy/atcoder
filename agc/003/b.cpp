315d3f3a1221f049759e7022114fe587db88433d
taketakeyyy <taketakeyyy@gmail.com>
2021-03-08 01:27:25 +0900

    solved

---
+++ agc/003/b.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#define _USE_MATH_DEFINES  // M_PI等のフラグ
+#define MOD 1000000007
+#define COUNTOF(array) (sizeof(array)/sizeof(array[0]))
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define intceil(a,b) (a+(b-1))/b
+using namespace std;
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<long,long>;
+const long long INF = LONG_LONG_MAX - 1001001001;
+void chmax(int& x, int y) { x = max(x,y); }
+void chmin(int& x, int y) { x = min(x,y); }
+
+
+void solve() {
+    // i-1のカードが奇数なら、1枚iのカードとペアを作って、あとはi-1のカードのみでペアを作る
+    // これを繰り返す
+    ll N; cin >> N;
+    map<ll,ll> mp;
+    for (int i=0; i<N; i++) {
+        cin >> mp[i];
+    }
+
+    if (N==1) {
+        cout << mp[0]/2 << endl;
+        return;
+    }
+
+    ll ans = 0;
+    ll pre = mp[0];
+    for (int i=1; i<N; i++) {
+        if (pre&1) {
+            if (mp[i]>=1) {
+                ans++;
+                pre -= 1;
+                mp[i] -= 1;
+            }
+        }
+        ans += pre/2;
+        pre = mp[i];
+    }
+    ans += pre/2;
+    cout << ans << endl;
+}
+
+
+int main() {
+    solve();
+    return 0;
+}
