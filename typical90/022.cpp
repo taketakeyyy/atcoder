e13fd76daa3b60a5474a6de1c54d71093dd9cfb3
taketakeyyy <taketakeyyy@gmail.com>
2021-06-21 11:38:50 +0900

    solved

---
+++ typical90/022.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define _USE_MATH_DEFINES  // M_PI等のフラグ
+#define MOD 1000000007
+#define COUNTOF(array) (sizeof(array)/sizeof(array[0]))
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define intceil(a,b) (a+(b-1))/b
+using namespace std;
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<long,long>;
+const long long INF = LONG_LONG_MAX - 1001001001001001;
+void chmax(int& x, int y) { x = max(x,y); }
+void chmin(int& x, int y) { x = min(x,y); }
+
+
+void solve() {
+    ll A, B, C; cin >> A >> B >> C;
+    ll r = gcd(A, gcd(B, C));
+    ll ans = A/r-1 + B/r-1 + C/r-1;
+    cout << ans << endl;
+}
+
+
+int main() {
+    solve();
+    return 0;
+}
