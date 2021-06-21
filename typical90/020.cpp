4d10a043e10417ee8990724c895d3cbf91f4e56b
taketakeyyy <taketakeyyy@gmail.com>
2021-06-21 10:50:04 +0900

    solved

---
+++ typical90/020.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define _USE_MATH_DEFINES  // M_PI等のフラグ
+#define MOD 1000000007
+#define COUNTOF(array) (sizeof(array)/sizeof(array[0]))
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define intceil(a,b) (a+(b-1))/b
+using namespace std;
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<long,long>;
+const long long INF = LONG_LONG_MAX - 1001001001001001;
+void chmax(int& x, int y) { x = max(x,y); }
+void chmin(int& x, int y) { x = min(x,y); }
+
+
+void solve() {
+    ll a, b, c; cin >> a >> b >> c;
+    // 13^17はlong long(9.0*10^18)で表現できる
+    ll r = 1;
+    for(int i=0; i<b; i++) {
+        r *= c;
+    }
+
+    if (a < r) {
+        cout << "Yes" << endl;
+    }
+    else {
+        cout << "No" << endl;
+    }
+}
+
+
+int main() {
+    solve();
+    return 0;
+}
