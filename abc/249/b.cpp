540fdeb8f2875e9e4a880ad1c7e9b0bd6e2123f5
taketakeyyy <taketakeyyy@gmail.com>
2022-05-21 22:56:05 +0900

    rename

---
+++ abc/249/b.cpp
@@ -0,0 +1,49 @@
+#define _USE_MATH_DEFINES  // M_PI等のフラグ
+#include <bits/stdc++.h>
+#define MOD 1000000007
+#define COUNTOF(array) (sizeof(array)/sizeof(array[0]))
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define intceil(a,b) ((a+(b-1))/b)
+using namespace std;
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<long,long>;
+const long long INF = LONG_LONG_MAX - 1001001001001001;
+void chmax(int& x, int y) { x = max(x,y); }
+void chmin(int& x, int y) { x = min(x,y); }
+
+
+void solve() {
+    string S; cin >> S;
+    bool existBig = false;
+    bool existSmall = false;
+    set<char> st;
+
+    for(char c: S) {
+        if (st.count(c)) {
+            cout << "No" << endl;
+            return;
+        }
+
+        st.insert(c);
+        if (65 <= c && c <= 90) {
+            existBig = true;
+        }
+        if (97 <= c && c <= 122) {
+            existSmall = true;
+        }
+    }
+
+    if (existBig && existSmall) {
+        cout << "Yes" << endl;
+    }
+    else {
+        cout << "No" << endl;
+    }
+}
+
+
+int main() {
+    solve();
+    return 0;
+}
