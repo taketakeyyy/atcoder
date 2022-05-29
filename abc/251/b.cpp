ae66844cfcae356f0bfff00c188fbd9ae1dfc6d3
taketakeyyy <taketakeyyy@gmail.com>
2022-05-29 21:29:54 +0900

    solved

---
+++ abc/251/b.cpp
@@ -0,0 +1,55 @@
+#define _USE_MATH_DEFINES  // M_PI等のフラグ
+#include <bits/stdc++.h>
+#define MOD 1000000007
+#define COUNTOF(array) (sizeof(array)/sizeof(array[0]))
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define intceil(a,b) ((a+(b-1))/b)
+using namespace std;
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<long,long>;
+const long long INF = LONG_LONG_MAX - 1001001001001001;
+void chmax(int& x, int y) { x = max(x,y); }
+void chmin(int& x, int y) { x = min(x,y); }
+
+
+void solve() {
+    ll N, W; cin >> N >> W;
+    vector<ll> A(N);
+    for(ll i=0; i<N; i++) {
+        cin >> A[i];
+    }
+
+    set<ll> st;
+    // 1個選ぶ
+    for(ll i=0; i<N; i++) {
+        ll w = A[i];
+        if (w <= W) st.insert(w);
+    }
+
+    // 2個選ぶ
+    for(ll i=0; i<N; i++) {
+        for(ll j=i+1; j<N; j++) {
+            ll w = A[i]+A[j];
+            if (w <= W) st.insert(w);
+        }
+    }
+
+    // 3個選ぶ
+    for(ll i=0; i<N; i++) {
+        for(ll j=i+1; j<N; j++) {
+            for(ll k=j+1; k<N; k++) {
+                ll w = A[i]+A[j]+A[k];
+                if (w <= W) st.insert(w);
+            }
+        }
+    }
+
+    cout << st.size() << endl;
+}
+
+
+int main() {
+    solve();
+    return 0;
+}
