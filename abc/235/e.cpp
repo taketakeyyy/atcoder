1193fc5551c89ce34e7fc2b990953f3223713c74
taketakeyyy <taketakeyyy@gmail.com>
2022-12-31 17:04:42 +0900

    solved

---
+++ abc/235/e.cpp
@@ -0,0 +1,155 @@
+#define _USE_MATH_DEFINES  // M_PI等のフラグ
+#include <bits/stdc++.h>
+#define MOD 1000000007
+#define COUNTOF(array) (sizeof(array)/sizeof(array[0]))
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define intceil(a,b) ((a+(b-1))/b)
+using namespace std;
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<long,long>;
+const long long INF = LONG_LONG_MAX - 1001001001001001;
+void chmax(int& x, int y) { x = max(x,y); }
+void chmin(int& x, int y) { x = min(x,y); }
+string vs = "URDL";  // 上右下左
+vector<ll> vy = { -1, 0, 1, 0 };
+vector<ll> vx = { 0, 1, 0, -1 };
+
+/***
+ *  UnionFindVerSize
+ *
+ * Descriptions:
+ *  UnionFindのサイズバージョン
+ *  グループの併合のとき、サイズが大きい方が、小さい方を併合する
+ *  競プロでは、ノードの個数やグループの最小値とかを求めるので、こちらを使うことが多い
+ *
+ * Args
+ *  N(T): N個のノードのUnionFindを作成する
+ *
+ * ならし計算量
+ *  O(α(N))
+ *    - α(N)はアッカーマン関数
+ *    - 相当小さい（logより小さい）
+ ***/
+template <typename T>
+struct UnionFindVerSize {
+    private:
+        vector<T> sizes;   // グループのサイズ
+        vector<T> parents;  // 親の番号を格納する。自分が親の場合は自分の番号になり、それがそのグループの番号になる
+        T group_num;  // 連結成分の個数
+
+    public:
+        UnionFindVerSize(T N=0): sizes(N,1), parents(N) {
+            for (T i=0; i<N; i++) {
+                parents[i] = i;
+            }
+            this->group_num = N;
+        }
+
+        T find_root(T x) {
+            /* ノード番号xの木の根（xがどのグループか）を求める */
+            if (this->parents[x] == x) return x;
+            this->parents[x] = this->find_root(this->parents[x]);  // 縮約
+            return this->parents[x];
+        }
+
+        void unite(T x, T y) {
+            /* ノード番号xとyが属する木を併合する（グループの併合） */
+            T gx = this->find_root(x); T gy = this->find_root(y);
+            if (gx == gy) return;
+
+            // 深い方が浅い方を併合する（木の偏りが減るので）
+            if (this->sizes[gx] < this->sizes[gy]) {
+                this->parents[gx] = gy;
+                this->sizes[gy] += this->sizes[gx];
+            }
+            else {
+                this->parents[gy] = gx;
+                this->sizes[gx] += this->sizes[gy];
+            }
+            this->group_num--;
+        }
+
+        T get_size(T x) {
+            /* ノード番号xが属するグループの深さを返す */
+            return this->sizes[this->find_root(x)];
+        }
+
+        bool is_same_group(T x, T y) {
+            /* ノード番号xとyが同じ集合に属するか否か */
+            return this->find_root(x) == this->find_root(y);
+        }
+
+        T get_group_num() {
+            /* 連結成分の個数を返す */
+            return this->group_num;
+        }
+
+        void print_parents() {
+            /* デバッグ用parentsの中身を出力する */
+            for (T i=0; i<this->parents.size(); i++) {
+                cout << this->parents[i] << endl;
+            }
+        }
+
+        void print_sizes() {
+            /* デバッグ用sizesの中身を出力する */
+            for (T i=0; i<this->sizes.size(); i++) {
+                cout << this->sizes[i] << endl;
+            }
+        }
+};
+
+
+struct edge {long long u, v, cost, idx; };  // 頂点uと頂点vを結ぶ無向辺（コスト：cost）
+bool operator<(const edge &a, const edge &b) {
+    return a.cost < b.cost;
+}
+
+void solve() {
+    ll N, M, Q; cin >> N >> M >> Q;
+    vector<edge> edges;
+    for(ll i=0; i<M; i++) {
+        ll a, b, c; cin >> a >> b >> c;
+        a--; b--;
+        edges.push_back(edge{a,b,c,-1});
+    }
+
+    // クエリ先読み
+    // map<edge,ll> query_map;  // クエリ辺が、何番目のクエリか // 重複の可能性があるので駄目！
+    for(ll q=0; q<Q; q++) {
+        ll u, v, w; cin >> u >> v >> w;
+        u--; v--;
+        edge e = edge{u,v,w,q};  // しょうがないので辺情報にクエリ番号を付与する
+        edges.push_back(e);
+    }
+
+    // クラスカル法で最小全域木を作る過程で、クエリ辺が連結になるならYes
+    // 「クラスカル法」 := 辺をコストの昇順でソートして、
+    // 順に「頂点uと頂点vが連結じゃないないら結合する」を繰り返せば最小全域木の完成
+    sort(edges.begin(), edges.end());
+    vector<bool> ans(Q, false);  // 答えを格納
+    UnionFindVerSize<ll> uf(N);
+    for(ll i=0; i<(ll)edges.size(); i++) {
+        edge e = edges[i];
+
+        if (e.idx != -1) {
+            // クエリ処理
+            ans[e.idx] = !uf.is_same_group(e.u, e.v);  // 連結じゃないならYes
+            continue;
+        }
+
+        if (!uf.is_same_group(e.u, e.v)) uf.unite(e.u, e.v);
+    }
+
+    // 出力
+    for(ll q=0; q<Q; q++) {
+        if (ans[q]) cout << "Yes" << endl;
+        else cout << "No" << endl;
+    }
+}
+
+int main() {
+    solve();
+    return 0;
+}
