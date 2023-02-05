557e91ac6e208f32c5d4e192819359edafef6be1
taketakeyyy <taketakeyyy@gmail.com>
2023-02-05 13:21:44 +0900

    solved

---
+++ abc/288/b.cpp
@@ -0,0 +1,38 @@
+#define _USE_MATH_DEFINES  // M_PI等のフラグ
+#include <bits/stdc++.h>
+#define MOD 1000000007
+#define COUNTOF(array) (sizeof(array)/sizeof(array[0]))
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define intceil(a,b) ((a+(b-1))/b)
+using namespace std;
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<long,long>;
+const long long INF = LONG_LONG_MAX - 1001001001001001;
+void chmax(int& x, int y) { x = max(x,y); }
+void chmin(int& x, int y) { x = min(x,y); }
+string vs = "URDL";  // 上右下左
+vector<ll> vy = { -1, 0, 1, 0 };
+vector<ll> vx = { 0, 1, 0, -1 };
+
+
+void solve() {
+    ll N, K; cin >> N >> K;
+    vector<string> S;
+    for(ll i=0; i<N; i++) {
+        string s; cin >> s;
+        if (S.size() != K) S.push_back(s);
+    }
+
+    sort(S.begin(), S.end());
+
+    for(ll i=0; i<(ll)S.size(); i++) {
+        cout << S[i] << endl;
+    }
+}
+
+
+int main() {
+    solve();
+    return 0;
+}
