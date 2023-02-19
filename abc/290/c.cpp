d102cd2b0c7108103f418d46ea6a338efb54e376
taketakeyyy <taketakeyyy@gmail.com>
2023-02-20 00:28:26 +0900

    solved

---
+++ abc/290/c.cpp
@@ -0,0 +1,46 @@
+#define _USE_MATH_DEFINES  // M_PI等のフラグ
+#include <bits/stdc++.h>
+#define MOD 1000000007
+#define COUNTOF(array) (sizeof(array)/sizeof(array[0]))
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define intceil(a,b) ((a+(b-1))/b)
+using namespace std;
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<long,long>;
+const long long INF = LONG_LONG_MAX - 1001001001001001;
+void chmax(int& x, int y) { x = max(x,y); }
+void chmin(int& x, int y) { x = min(x,y); }
+string vs = "URDL";  // 上右下左
+vector<ll> vy = { -1, 0, 1, 0 };
+vector<ll> vx = { 0, 1, 0, -1 };
+
+
+void solve() {
+    ll N, K; cin >> N >> K;
+    vector<ll> A(N);
+    for(ll i=0; i<N; i++) cin >> A[i];
+    sort(A.begin(), A.end());
+    ll mex = 0;
+    for(ll i=0; i<N; i++) {
+        if (K == 0) break;
+        if (mex == A[i]) {
+            mex++;
+            K--;
+            continue;
+        }
+        else if (mex > A[i]) {
+            continue;
+        }
+        else {
+            break;
+        }
+    }
+    cout << mex << endl;
+}
+
+
+int main() {
+    solve();
+    return 0;
+}
