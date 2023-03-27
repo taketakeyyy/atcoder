ede4c1c89d9a6f655f31b70e88eda7413e14715e
taketakeyyy <taketakeyyy@gmail.com>
2023-03-27 23:18:34 +0900

    solved

---
+++ agc/023/a.cpp
@@ -0,0 +1,50 @@
+#define _USE_MATH_DEFINES  // M_PI等のフラグ
+#include <bits/stdc++.h>
+#define MOD 1000000007
+#define COUNTOF(array) (sizeof(array)/sizeof(array[0]))
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define intceil(a,b) ((a+(b-1))/b)
+using namespace std;
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<long,long>;
+const long long INF = LONG_LONG_MAX - 1001001001001001;
+void chmax(int& x, int y) { x = max(x,y); }
+void chmin(int& x, int y) { x = min(x,y); }
+string vs = "URDL";  // 上右下左
+vector<ll> vy = { -1, 0, 1, 0 };
+vector<ll> vx = { 0, 1, 0, -1 };
+
+
+void solve() {
+    ll N; cin >> N;
+    vector<ll> A(N+1);
+    for(ll i=1; i<=N; i++) {
+        cin >> A[i];
+    }
+
+    // 累積和
+    vector<ll> r(N+1);
+    for(ll i=1; i<=N; i++) {
+        r[i] = r[i-1] + A[i];
+    }
+
+    // 分布をとる
+    map<ll,ll> cnt;
+    for(ll i=0; i<=N; i++) {
+        cnt[r[i]]++;
+    }
+
+    // 分布からnC2を足し合わせる
+    ll ans = 0;
+    for(auto[key, val]: cnt) {
+        ans += val*(val-1)/2; // nC2
+    }
+    cout << ans << endl;
+}
+
+
+int main() {
+    solve();
+    return 0;
+}
--- agc/023/a.py
+++
@@ -1,5 +0,0 @@
-
-if __name__ == "__main__":
-    N = int(input())
-    A = list(map(int, input().split()))
-    
