b49c6513131b59c8e13fb58e2f54c6d67daf90c6
taketakeyyy <taketakeyyy@gmail.com>
2023-04-01 23:16:42 +0900

    solved

---
+++ abc/296/b.cpp
@@ -0,0 +1,44 @@
+#define _USE_MATH_DEFINES  // M_PI等のフラグ
+#include <bits/stdc++.h>
+#define MOD 1000000007
+#define COUNTOF(array) (sizeof(array)/sizeof(array[0]))
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define intceil(a,b) ((a+(b-1))/b)
+using namespace std;
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<long,long>;
+const long long INF = LONG_LONG_MAX - 1001001001001001;
+void chmax(int& x, int y) { x = max(x,y); }
+void chmin(int& x, int y) { x = min(x,y); }
+string vs = "URDL";  // 上右下左
+vector<ll> vy = { -1, 0, 1, 0 };
+vector<ll> vx = { 0, 1, 0, -1 };
+
+
+void solve() {
+    vector<string> grid(8);
+    for(ll i=0; i<8; i++) {
+        cin >> grid[i];
+    }
+
+    ll ansh, answ;
+    for(ll i=0; i<8; i++) {
+        for(ll j=0; j<8; j++) {
+            if (grid[i][j] == '*') {
+                answ = j;
+                ansh = 8-(i+1);
+            }
+        }
+    }
+
+    char h = '1' + ansh;
+    char w = 'a' + answ;
+    cout << w << h << endl;
+}
+
+
+int main() {
+    solve();
+    return 0;
+}
