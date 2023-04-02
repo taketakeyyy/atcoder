698381560b28c14b5fb69216e867f07655fd51de
taketakeyyy <taketakeyyy@gmail.com>
2023-04-02 23:48:41 +0900

    solved

---
+++ other_contests/TENKA1-2018/b.cpp
@@ -0,0 +1,42 @@
+#define _USE_MATH_DEFINES  // M_PI等のフラグ
+#include <bits/stdc++.h>
+#define MOD 1000000007
+#define COUNTOF(array) (sizeof(array)/sizeof(array[0]))
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define intceil(a,b) ((a+(b-1))/b)
+using namespace std;
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<long,long>;
+const long long INF = LONG_LONG_MAX - 1001001001001001;
+void chmax(int& x, int y) { x = max(x,y); }
+void chmin(int& x, int y) { x = min(x,y); }
+string vs = "URDL";  // 上右下左
+vector<ll> vy = { -1, 0, 1, 0 };
+vector<ll> vx = { 0, 1, 0, -1 };
+
+
+void solve() {
+    ll A, B, K; cin >> A >> B >> K;
+
+    for(ll k=0; k<K; k++) {
+        if (k%2 == 0) {
+            if (A%2 == 1) A--;
+            A /= 2;
+            B += A;
+        }
+        else {
+            if (B%2 == 1) B--;
+            B /= 2;
+            A += B;
+        }
+    }
+
+    cout << A << " " << B << endl;
+}
+
+
+int main() {
+    solve();
+    return 0;
+}
