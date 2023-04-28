c00796ec2126372e598850c454071bda1591cfa8
taketakeyyy <taketakeyyy@gmail.com>
2023-04-28 23:46:06 +0900

    solved

---
+++ other_contests/M-SOLUTIONS2020/b.cpp
@@ -0,0 +1,39 @@
+#define _USE_MATH_DEFINES  // M_PI等のフラグ
+#include <bits/stdc++.h>
+#define MOD 1000000007
+#define COUNTOF(array) (sizeof(array)/sizeof(array[0]))
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define intceil(a,b) ((a+(b-1))/b)
+using namespace std;
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<long,long>;
+const long long INF = LONG_LONG_MAX - 1001001001001001;
+void chmax(int& x, int y) { x = max(x,y); }
+void chmin(int& x, int y) { x = min(x,y); }
+string vs = "URDL";  // 上右下左
+vector<ll> vy = { -1, 0, 1, 0 };
+vector<ll> vx = { 0, 1, 0, -1 };
+
+
+void solve() {
+    ll A, B, C; cin >> A >> B >> C;
+    ll K; cin >> K;
+    for(ll k=0; k<K; k++) {
+        if (A >= B) {
+            B *= 2;
+        }
+        else if (B >= C){
+            C *= 2;
+        }
+    }
+
+    if (A < B && B < C) cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
+
+
+int main() {
+    solve();
+    return 0;
+}
