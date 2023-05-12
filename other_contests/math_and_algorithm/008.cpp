d6ed954fa75a73aac65ec1eb400f1d8b9baba7e1
taketakeyyy <taketakeyyy@gmail.com>
2023-05-13 07:42:06 +0900

    rename

---
+++ other_contests/math_and_algorithm/008.cpp
@@ -0,0 +1,37 @@
+#define _USE_MATH_DEFINES  // M_PI等のフラグ
+#include <bits/stdc++.h>
+#define MOD 1000000007
+#define COUNTOF(array) (sizeof(array)/sizeof(array[0]))
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define intceil(a,b) ((a+(b-1))/(b))
+using namespace std;
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<long,long>;
+const long long INF = LONG_LONG_MAX - 1001001001001001;
+void chmax(int& x, int y) { x = max(x,y); }
+void chmin(int& x, int y) { x = min(x,y); }
+string vs = "URDL";  // 上右下左
+vector<ll> vy = { -1, 0, 1, 0 };
+vector<ll> vx = { 0, 1, 0, -1 };
+
+
+void solve() {
+    ll N, S; cin >> N >> S;
+
+    ll ans = 0;
+    for(ll i=1; i<=N; i++) {
+        if (i > S) break;
+        for(ll j=1; j<=N; j++) {
+            if (i+j > S) break;
+            ans++;
+        }
+    }
+    cout << ans << endl;
+}
+
+
+int main() {
+    solve();
+    return 0;
+}
