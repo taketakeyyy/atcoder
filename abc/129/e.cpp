7ee51a9b56ca1200bd15aac09769c377b2c0b950
taketakeyyy <taketakeyyy@gmail.com>
2023-09-04 16:22:27 +0900

    solved

---
+++ abc/129/e.cpp
@@ -0,0 +1,99 @@
+#define _USE_MATH_DEFINES  // M_PI等のフラグ
+#include <bits/stdc++.h>
+#define MOD 1000000007
+#define COUNTOF(array) (sizeof(array)/sizeof(array[0]))
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define intceil(a,b) ((a+(b-1))/(b))
+using namespace std;
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<long,long>;
+const long long INF = LONG_LONG_MAX - 1001001001001001;
+void chmax(int& x, int y) { x = max(x,y); }
+void chmin(int& x, int y) { x = min(x,y); }
+string vs = "URDL";  // 上右下左
+vector<ll> vy = { -1, 0, 1, 0 };
+vector<ll> vx = { 0, 1, 0, -1 };
+
+/** 自動でMODをとるModInt型
+ *
+ * Usage
+ *   // 基本的な使い方
+ *   mint a, b;
+ *   a = 100;
+ *   b = a + MOD;
+ *   // istreamとostreamをオーバロードしてるので、mint型も普通に入出力できる
+ *   cout << b << endl;  // 100
+ *   // 明示的にlong long型を出力するときは.xを参照する
+ *   cout << b.x << endl;  // 100
+ *
+ * References
+ *  https://youtu.be/L8grWxBlIZ4?t=9858
+ *  https://youtu.be/ERZuLAxZffQ?t=4807 : optimize
+ *  https://youtu.be/8uowVvQ_-Mo?t=1329 : division
+ */
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%MOD+MOD)%MOD){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= MOD) x -= MOD;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += MOD-a.x) >= MOD) x -= MOD;
+    return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= MOD; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+
+  // for prime MOD
+  mint inv() const { return pow(MOD-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+  bool operator<=(const mint a) const { return mint(*this) <= a; }
+  bool operator<(const mint a) const { return mint(*this) < a; }
+  bool operator>=(const mint a) const { return mint(*this) >= a; }
+  bool operator>(const mint a) const { return mint(*this) > a; }
+  bool operator==(const mint a) const { return mint(*this) == a; }
+};
+istream& operator>>(istream& is, mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+void solve() {
+    string L; cin >> L;
+    const ll MXL = L.size();
+
+    // dp[i][j] := 上位i桁目まで見て、L未満フラグがjのときの、(a,b)組の場合の数
+    vector dp(MXL, vector<mint>(2, 0));
+    dp[0][0] = 2; // (1,0),(0,1)の2通り
+    dp[0][1] = 1; // (0,0)の1通り
+    for(ll i=1; i<MXL; i++) {
+        if (L[i] == '0') {
+            dp[i][0] += dp[i-1][0]; // (0,0)の1通り
+            dp[i][1] += dp[i-1][1]*3; // (0,0),(1,0),(0,1)の3通り
+        }
+        else { // L[i] == '1'
+            dp[i][0] += dp[i-1][0]*2; // (1,0),(0,1)の2通り
+            dp[i][1] += dp[i-1][0]; // (0,0)の1通り
+            dp[i][1] += dp[i-1][1]*3; // (0,0),(1,0),(0,1)の3通り
+        }
+    }
+    mint ans = dp[MXL-1][0] + dp[MXL-1][1];
+    cout << ans << endl;
+}
+
+
+int main() {
+    solve();
+    return 0;
+}
