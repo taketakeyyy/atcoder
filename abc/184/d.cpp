e647a0753e4b2e0186d7a675362336c49c8d16db
taketakeyyy <taketakeyyy@gmail.com>
2023-10-30 22:35:59 +0900

    solved

--- abc/184/d.cpp
+++ abc/184/d.cpp
@@ -58,8 +58,8 @@ void solve(){
 
 
 
+// 解説AC
 void solve2() {
-    /* 解説AC */
     const int X = 100;
     // dp[i][j][k]:=金貨i枚、銀貨j枚、銅貨k枚ある状態からの期待値
     //             = dp[i+1][j][k]*(i/(i+j+k))
@@ -87,7 +87,51 @@ void solve2() {
     return;
 }
 
-int main(int argc, char const *argv[]){
-    solve2();
+
+// 期待値DPの解法
+void solve3() {
+    ll A, B, C; cin >> A >> B >> C;
+
+    // dp[a][b][c] := 現在の硬貨の枚数が(a,b,c)の状態での、終了するまでの操作回数の期待値
+    vector dp(101, vector<vector<double>>(101, vector<double>(101, 0)));
+
+    // dpを降順に埋める
+    for(ll a=99; a>=A; a--) {
+        for(ll b=99; b>=B; b--) {
+            for(ll c=99; c>=C; c--) {
+                double p1 = (double)a/(a+b+c);
+                double p2 = (double)b/(a+b+c);
+                double p3 = (double)c/(a+b+c);
+                dp[a][b][c] += (1+dp[a+1][b][c])*p1 + (1+dp[a][b+1][c])*p2 + (1+dp[a][b][c+1])*p3;
+            }
+        }
+    }
+    printf("%.10lf\n", dp[A][B][C]);
+}
+
+// メモ化再帰の解法
+void solve4() {
+    ll A, B, C; cin >> A >> B >> C;
+
+    // メモ化再帰 f(a,b,c) := 現在の硬貨の枚数が(a,b,c)の状態での、終了するまでの操作回数の期待値
+    vector memo(101, vector<vector<double>>(101, vector<double>(101, -1)));
+    auto f = [&](auto self, ll a, ll b, ll c) -> double {
+        if(a==100 || b==100 || c==100) return 0;
+        if (memo[a][b][c] != -1) return memo[a][b][c];
+
+        double p1 = (double)a/(a+b+c);
+        double p2 = (double)b/(a+b+c);
+        double p3 = (double)c/(a+b+c);
+        return memo[a][b][c] = (1+self(self,a+1,b,c))*p1 + (1+self(self,a,b+1,c))*p2 + (1+self(self,a,b,c+1))*p3;
+    };
+
+    // 出力
+    printf("%.10lf\n", f(f,A,B,C));
+}
+
+int main(){
+    // solve2();
+    solve3();
+    // solve4();
     return 0;
 }
