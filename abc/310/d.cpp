6d0d8691e44afb7361c2eee1c09362e8eda2b89b
taketakeyyy <taketakeyyy@gmail.com>
2024-03-23 18:04:04 +0900

    solved

--- abc/310/d.cpp
+++ abc/310/d.cpp
@@ -9,26 +9,69 @@ using ll = long long;
 using pii = pair<int,int>;
 using pll = pair<long,long>;
 const long long INF = LONG_LONG_MAX - 1001001001001001;
-void chmax(int& x, int y) { x = max(x,y); }
-void chmin(int& x, int y) { x = min(x,y); }
+void chmax(ll& x, ll y) { x = max(x,y); }
+void chmin(ll& x, ll y) { x = min(x,y); }
 string vs = "URDL";  // 上右下左
 vector<ll> vy = { -1, 0, 1, 0 };
 vector<ll> vx = { 0, 1, 0, -1 };
 
 
-// 解説AC
-// i人目の人は、既存のチームに入るか、新しいチームを作るか
 void solve() {
     ll N, T, M; cin >> N >> T >> M;
-    vector<pair<ll,ll>> AB(M);
-    vector<vector<ll>> G(N);
+    vector<ll> A(M), B(M);
     for(ll i=0; i<M; i++) {
-        ll a, b; cin >> a >> b;
-        a--; b--;
-        AB[i] = {a,b};
-        G[a].push_back(b);
-        G[b].push_back(a);
+        cin >> A[i] >> B[i];
+        A[i]--; B[i]--;
     }
+
+    // n!を計算
+    auto kaijo = [&](ll n) {
+        ll res = 1;
+        for(ll i=1; i<=n; i++) res *= i;
+        return res;
+    };
+
+    // チームstは仲の悪い人間が含まれていない、平和なチームか？
+    auto is_team_ok = [&](set<ll> &st) {
+        for(ll i=0; i<M; i++) {
+            if (st.count(A[i]) and st.count(B[i])) {
+                return false;
+            }
+        }
+        return true;
+    };
+
+    // O(T 3^N (N+M))
+    // dp[t][s] := t番目のチームを決めるとき、すでに使った選手の状態がsのときの、場合の数
+    vector<ll> dp(1<<N, 0);
+    dp[0] = 1;
+    for(ll t=0; t<T; t++) {
+        vector<ll> newdp(1<<N, 0);
+
+        // 今回の状態s
+        for(ll s=1; s<1<<N; s++) {
+            set<ll> team;
+            for(ll d=0; d<N; d++) {
+                if (s>>d&1) {
+                    team.insert(d);
+                }
+            }
+
+            if (!is_team_ok(team)) continue;
+
+            // sを反転させたfs
+            ll fs = ~s; fs &= (1<<N)-1;
+            for(ll t=fs;; t=(t-1)&fs) { // tはfsの部分集合
+                // tからs|tへ遷移する
+                newdp[s|t] += dp[t];
+                if (t==0) break;
+            }
+        }
+        swap(newdp, dp);
+    }
+
+    // 答え
+    cout << dp[(1<<N)-1]/kaijo(T) << endl;
 }
 
 
