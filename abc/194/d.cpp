327e0339f113f15f3985f29d192550346de7cada
taketakeyyy <taketakeyyy@gmail.com>
2024-04-15 00:08:16 +0900

    fix

--- abc/194/d.cpp
+++ abc/194/d.cpp
@@ -29,8 +29,36 @@ void solve() {
     printf("%.10lf\n",ans);
 }
 
+// AC
+void solve2() {
+    ll N; cin >> N;
+
+    // 現在n個の頂点が連結している状態での、終了するまでの操作回数の期待値dp[n]
+    vector<double> dp(N+1);
+    dp[N] = 0;
+    for(ll n=N-1; n>=1; n--) {
+        dp[n] = dp[n+1] + (double)N/(N-n);
+    }
+    printf("%.10lf\n",dp[1]);
+}
+
+// WA
+// solve2のように「終了するまでの」操作回数の期待値としないと、うまく動かないんだよなあ。理由はあまりわかってない。
+void solve3() {
+    ll N; cin >> N;
+
+    // 現在n個の頂点が連結している状態になるまでの、操作回数の期待値dp[n]
+    vector<double> dp(N+1);
+    dp[1] = 0;
+    for(ll n=1; n<=N-1; n++) {
+        dp[n+1] = dp[n] - (double)N/(N-n);
+    }
+    printf("%.10lf\n",dp[N]);
+}
 
 int main() {
-    solve();
+    // solve();
+    solve2();
+    // solve3();
     return 0;
 }
