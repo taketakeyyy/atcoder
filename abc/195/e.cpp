94813044cb7d2275ece9bf309f5d2cff666bc36e
taketakeyyy <taketakeyyy@gmail.com>
2024-04-16 05:32:12 +0900

    fix

--- abc/195/e.cpp
+++ abc/195/e.cpp
@@ -1,56 +1,60 @@
-#include <bits/stdc++.h>
 #define _USE_MATH_DEFINES  // M_PI等のフラグ
+#include <bits/stdc++.h>
 #define MOD 1000000007
 #define COUNTOF(array) (sizeof(array)/sizeof(array[0]))
 #define rep(i,n) for (int i = 0; i < (n); ++i)
-#define intceil(a,b) (a+(b-1))/b
+#define intceil(a,b) ((a+(b-1))/(b))
 using namespace std;
 using ll = long long;
 using pii = pair<int,int>;
 using pll = pair<long,long>;
-const long long INF = LONG_LONG_MAX - 1001001001;
-void chmax(int& x, int y) { x = max(x,y); }
-void chmin(int& x, int y) { x = min(x,y); }
+const long long INF = LONG_LONG_MAX - 1001001001001001;
+void chmax(ll& x, ll y) { x = max(x,y); }
+void chmin(ll& x, ll y) { x = min(x,y); }
+string vs = "URDL";  // 上右下左
+vector<ll> vy = { -1, 0, 1, 0 };
+vector<ll> vx = { 0, 1, 0, -1 };
 
 
+// 2人ゲームの基本の考え
+// 後ろから考える（最終状態から考える）
+// 今回の手番がxのときにxが勝つには、盤面Sから遷移可能な盤面S'のうち、ひとつでも勝ち盤面があるなら、その盤面に行けば勝てるので、Sも勝ち盤面になる
+// 逆に、ひとつも勝ち盤面がないなら、どの盤面に遷移しても負けなので、Sも負け盤面になる
 void solve() {
-    // ゲームの最適行動⇒後ろから考える
-    // dp[i][j] := i文字目まで決まっていて、mod7がjのときの勝敗
-    // dp[i+1] は一個前のoldを使って更新していけばいいので、実質dp[j]で済む
-    // プレイヤーはS[i]を選ぶか、0を選ぶかのどちらか
-    // MODの世界で考えるので、d桁目を見るときに10^d倍を足すが、mod7してOK
     ll N; cin >> N;
     string S; cin >> S;
     string X; cin >> X;
-    vector<ll> dp(7);  // dp[j] := mod7がjのときの勝敗
-    dp[0] = 1;  // 1:高橋の勝ち 0:青木の勝ち
-    ll ten = 1;
-    for (int i=N-1; i>=0; i--) {  // 後ろから考える（N-1文字目まで決まっている状態から考える）
-        vector<ll> old(7);  // 1つ前の状態のdpテーブル
-        swap(dp, old);  // 新しいdpをこれから計算したいので、現在のdpの状態をoldに記録し、dpを初期化する
-        for(int j=0; j<7; j++) {
-            // dp[j]を求める
-            ll nj = (j+ten*(S[i]-'0'))%7;  // S[i]を選んだときのmod7の値
-            if (X[i]=='T') {
-                // 高橋のターンならば
-                // dp[i][j] = dp[i+1][j] | dp[i+1][nj]   と同等のことをしている
-                dp[j] = old[j] | old[nj];  // dp[j] = (0を選んだ場合の勝敗) | (S[i]を選んだ場合の勝敗) （どちらかで高橋が勝てるなら、今回も勝てる）
+
+    // dp[i][j] := i番目までの文字まで見て、10進数で表された数(mod 7)がjのとき、高橋が勝つかどうか
+    vector<bool> dp(7, false);
+    dp[0] = true;
+    ll pow10 = 1;
+    for(ll i=N-1; i>=0; i--) {
+        vector<bool> newdp(7, false);
+        ll a = S[i]-'0'; // 今回の値
+        a *= pow10;
+        a %= 7;
+        for(ll j=0; j<7; j++) {
+            ll nj = a + j;
+            nj %= 7;
+            if (X[i]=='T') { // 高橋のターン
+                // 高橋が勝つには、盤面Sから遷移可能な盤面S'のうち、ひとつでも勝ち盤面があるなら、その盤面に行けば勝てるので、Sも勝ち盤面になる
+                // jから遷移可能な盤面：jとnj
+                newdp[j] = dp[j] | dp[nj];
             }
-            else {
-                // 青木のターンならば
-                dp[j] = old[j] & old[nj];  // dp[j] = (0を選んだ場合の勝敗) & (S[i]を選んだ場合の勝敗)　（どちらかで青木が勝てるなら、今回も勝てる）
+            else {// 青木のターン
+                // 青木が勝つには、盤面Sから遷移可能な盤面S'のうち、ひとつでも勝ち盤面があるなら、その盤面に行けば勝てるので、Sも勝ち盤面になる
+                // jから遷移可能な盤面:jとnj
+                newdp[j] = !(!dp[j] | !dp[nj]);
             }
         }
-
-        ten = (ten*10)%7;
+        pow10 *= 10; pow10 %= 7;
+        swap(newdp, dp);
     }
 
-    if (dp[0]==1) {
-        cout << "Takahashi" << endl;
-    }
-    else {
-        cout << "Aoki" << endl;
-    }
+    // 答え
+    if (dp[0]) cout << "Takahashi" << endl;
+    else cout << "Aoki" << endl;
 }
 
 
