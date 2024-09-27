29e62c9cf9908f851d9c456bce2fcb03b538bedc
taketakeyyy <taketakeyyy@gmail.com>
2024-09-27 14:28:48 +0900

    solved

---
+++ other_contests/oupc2023-day1/b.cpp
@@ -0,0 +1,114 @@
+#define _USE_MATH_DEFINES  // M_PI等のフラグ
+#include <bits/stdc++.h>
+#define MOD 998244353
+#define COUNTOF(array) (sizeof(array)/sizeof(array[0]))
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define intceil(a,b) ((a+(b-1))/(b))
+using namespace std;
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<long,long>;
+const long long INF = LONG_LONG_MAX - 1'001'001'001'001'001;
+void chmax(ll& x, ll y) { x = max(x,y); }
+void chmin(ll& x, ll y) { x = min(x,y); }
+string vs = "URDL";  // 上右下左
+vector<ll> vy = { -1, 0, 1, 0 };
+vector<ll> vx = { 0, 1, 0, -1 };
+//ordered_set
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+template<typename T>
+using ordered_set = tree<T, null_type, std::less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+/** 自動でMODをとるModInt型
+ *
+ * Usage
+ *   // 基本的な使い方
+ *   mint a, b;
+ *   a = 100;
+ *   b = a + MOD;
+ *   // istreamとostreamをオーバロードしてるので、mint型も普通に入出力できる
+ *   cout << b << endl;  // 100
+ *   // 明示的にlong long型を出力するときは.xを参照する
+ *   cout << b.x << endl;  // 100
+ *
+ * References
+ *  https://youtu.be/L8grWxBlIZ4?t=9858
+ *  https://youtu.be/ERZuLAxZffQ?t=4807 : optimize
+ *  https://youtu.be/8uowVvQ_-Mo?t=1329 : division
+ */
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x=0):x((x%MOD+MOD)%MOD){}
+    mint operator-() const { return mint(-x);}
+    mint& operator+=(const mint a) {
+        if ((x += a.x) >= MOD) x -= MOD;
+        return *this;
+    }
+    mint& operator-=(const mint a) {
+        if ((x += MOD-a.x) >= MOD) x -= MOD;
+        return *this;
+    }
+    mint& operator*=(const mint a) { (x *= a.x) %= MOD; return *this;}
+    mint operator+(const mint a) const { return mint(*this) += a;}
+    mint operator-(const mint a) const { return mint(*this) -= a;}
+    mint operator*(const mint a) const { return mint(*this) *= a;}
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t>>1);
+        a *= a;
+        if (t&1) a *= *this;
+        return a;
+    }
+
+    // for prime MOD
+    mint inv() const { return pow(MOD-2);}
+    mint& operator/=(const mint a) { return *this *= a.inv();}
+    mint operator/(const mint a) const { return mint(*this) /= a;}
+    bool operator<=(const mint a) const { return mint(*this) <= a; }
+    bool operator<(const mint a) const { return mint(*this) < a; }
+    bool operator>=(const mint a) const { return mint(*this) >= a; }
+    bool operator>(const mint a) const { return mint(*this) > a; }
+    bool operator==(const mint a) const { return mint(*this) == a; }
+};
+istream& operator>>(istream& is, mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+
+void solve() {
+    string S; cin >> S;
+    ll N = S.size();
+
+    // 余事象：ゴママヨでないものを数え上げる
+    // まずは、同じ文字が連続するところで分割する
+    vector<string> divided;
+    string s = "";
+    for(ll i=0; i<N-1; i++) {
+        s.push_back(S[i]);
+        if (S[i]==S[i+1]) {
+            divided.push_back(s);
+            s.clear();
+        }
+    }
+    s.push_back(S[N-1]);
+    divided.push_back(s);
+
+    // 各分割文字列sにおいて、ゴママヨでないものを数え上げる
+    mint notAns = 0;
+    for(string s: divided) {
+        ll n = s.size();
+        notAns += ((1+n)*n)/2;
+    }
+
+    // 答え
+    mint ans = ((1+N)*N)/2;
+    ans -= notAns;
+    cout << ans << endl;
+}
+
+
+int main() {
+    solve();
+    return 0;
+}
