6181521b965f9284f344dfd6ac812c1fe5db4d78
taketakeyyy <taketakeyyy@gmail.com>
2024-09-28 20:57:25 +0900

    solved

---
+++ abc/342/b.cpp
@@ -0,0 +1,44 @@
+#define _USE_MATH_DEFINES  // M_PI等のフラグ
+#include <bits/stdc++.h>
+#define MOD 1000000007
+#define COUNTOF(array) (sizeof(array)/sizeof(array[0]))
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define intceil(a,b) ((a+(b-1))/(b))
+using namespace std;
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<long,long>;
+const long long INF = LONG_LONG_MAX - 1'001'001'001'001'001;
+void chmax(ll& x, ll y) { x = max(x,y); }
+void chmin(ll& x, ll y) { x = min(x,y); }
+string vs = "URDL";  // 上右下左
+vector<ll> vy = { -1, 0, 1, 0 };
+vector<ll> vx = { 0, 1, 0, -1 };
+//ordered_set
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+template<typename T>
+using ordered_set = tree<T, null_type, std::less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve() {
+    ll N; cin >> N;
+    vector<ll> man(N+1);
+    for(ll i=0; i<N; i++) {
+        ll p; cin >> p;
+        man[p] = i+1;
+    }
+
+    ll Q; cin >> Q;
+    for(ll i=0; i<Q; i++) {
+        ll a, b; cin >> a >> b;
+        if (man[a] > man[b]) cout << b << endl;
+        else cout << a << endl;
+    }
+}
+
+
+int main() {
+    solve();
+    return 0;
+}
