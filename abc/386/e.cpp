f45a8a3db0611225da7d0ef50e2b4ad71eefa4de
taketakeyyy <taketakeyyy@gmail.com>
2025-01-03 12:48:44 +0900

    solved

---
+++ abc/386/e.cpp
@@ -0,0 +1,61 @@
+#define _USE_MATH_DEFINES  // M_PI等のフラグ
+#include <bits/stdc++.h>
+#define MOD 1000000007
+#define COUNTOF(array) (sizeof(array)/sizeof(array[0]))
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define intceil(a,b) ((a+(b-1))/(b))
+using namespace std;
+using ll = long long;
+const long long INF = LONG_LONG_MAX - 1'001'001'001'001'001;
+void chmax(ll& x, ll y) { x = max(x,y); }
+void chmin(ll& x, ll y) { x = min(x,y); }
+string vs = "URDL";  // 上右下左
+vector<ll> vy = { -1, 0, 1, 0 };
+vector<ll> vx = { 0, 1, 0, -1 };
+//ordered_set
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+template<typename T>
+using ordered_set = tree<T, null_type, std::less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// 初項a, 末項l, 項数n のときの等差数列の和
+ll tousa1(const ll &a, const ll &l, const ll &n) { return ((a+l)*n)/2; }
+// 初項a, 公差d, 項数n のときの等差数列の和
+ll tousa2(const ll &a, const ll &d, const ll &n) { return ((2*a+(n-1)*d)*n)/2; }
+
+
+void solve() {
+    ll N, K; cin >> N >> K;
+    vector<ll> A(N);
+    for(ll i=0; i<N; i++) cin >> A[i];
+
+    ll ans = 0;
+    auto dfs = [&](auto dfs, ll res, ll depth, ll k) -> void {
+        // 終了条件
+        if (k == 0) {
+            chmax(ans, res);
+            return;
+        }
+        if (depth == N) {
+            return;
+        }
+
+        // 次の探索
+        dfs(dfs, res, depth+1, k); // 選ばない場合
+        dfs(dfs, res^A[depth], depth+1, k-1); // 選ぶ場合
+    };
+    if (K <= N-K) {
+        dfs(dfs, 0, 0, K);
+    }
+    else {
+        ll res = 0;
+        for(ll i=0; i<N; i++) res ^= A[i];
+        dfs(dfs, res, 0, N-K);
+    }
+    cout << ans << endl;
+}
+
+
+int main() {
+    solve();
+    return 0;
+}
